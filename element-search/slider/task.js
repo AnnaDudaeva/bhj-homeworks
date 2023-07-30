1ecbffdef1d1895e45888424311d2ca60151994f
Анна Дудаева <ha-beno09@yandex.ru>
2023-07-30 10:31:53 +0300

    Доработка задачи 3

--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -1,20 +1,20 @@
 const prev = document.querySelector('.slider__arrow_prev');
 const next = document.querySelector('.slider__arrow_next');
 const slider__item = document.querySelectorAll('.slider__item');
-let arr = Array.from(slider__item);
+let array = Array.from(slider__item);
 
 let sliderActive = 0;
 
 prev.onclick = function() {
-    let activeSlide = arr.findIndex((item, idx) => item.className.includes('slider__item_active'));
-    arr[activeSlide].classList.toggle("slider__item_active");
-    activeSlide = (activeSlide == 0) ? arr.length - 1 : activeSlide - 1;
-    arr[activeSlide].classList.toggle("slider__item_active");
+    let activeSlide = array.findIndex((item, idx) => item.className.includes('slider__item_active'));
+    array[activeSlide].classList.toggle("slider__item_active");
+    activeSlide = (activeSlide == 0) ? array.length - 1 : activeSlide - 1;
+    array[activeSlide].classList.toggle("slider__item_active");
   };
   
   next.onclick = function() {
-    let activeSlide = arr.findIndex((item, idx) => item.className.includes('slider__item_active'));
-    arr[activeSlide].classList.toggle("slider__item_active");
-    activeSlide = (activeSlide == arr.length - 1) ? 0 : activeSlide + 1;;
-    arr[activeSlide].classList.toggle("slider__item_active");
+    let activeSlide = array.findIndex((item, idx) => item.className.includes('slider__item_active'));
+    array[activeSlide].classList.toggle("slider__item_active");
+    activeSlide = (activeSlide == array.length - 1) ? 0 : activeSlide + 1;;
+    array[activeSlide].classList.toggle("slider__item_active");
   };
