bf2deaa1fee5b05ab1c79ab46d3b2d9272e66733
Анна Дудаева <ha-beno09@yandex.ru>
2023-07-30 10:27:03 +0300

    Доработка 3 задачи

--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -3,35 +3,18 @@ const arrowPrev = document.querySelector(".slider__arrow_prev");
 const arrowNext = document.querySelector(".slider__arrow_next");
 const dots = document.querySelectorAll(".slider__dot");
 
-let sliderActive = 0;
+//let sliderActive = 0;
 
-
-    arrowPrev.onclick = () => {
-        if (sliderActive - 1 < 0) {
-        sliderActive = slider.length - 1;
-        } else {
-            sliderActive -= 1;
-        }
-        changeSlide(sliderActive);
-    }
-    
-    arrowNext.onclick = () => {
-        if (sliderActive + 1 === slider.length) {
-        sliderActive = 0;
-        } else {
-            sliderActive += 1;
-        }
-        changeSlide(sliderActive);
-    }
-
-dots[sliderActive].classList.add('slider__dot_active');
-[...dots].forEach((item, i) => item.onclick = () => {
-    changeSlide(i);
-});
-
-function changeSlide(i) {
-    [...slider].forEach((element) => element.classList.remove('slider__item_active'));
-    [...dots].forEach((item) => item.classList.remove('slider__dot_active'));
-    slider[i].classList.add('slider__item_active');
-    dots[i].classList.add('slider__dot_active');
-}
+prev.onclick = function() {
+    let activeSlide = arr.findIndex((item, idx) => item.className.includes('slider__item_active'));
+    arr[activeSlide].classList.toggle("slider__item_active");
+    activeSlide = (activeSlide == 0) ? arr.length - 1 : activeSlide - 1;
+    arr[activeSlide].classList.toggle("slider__item_active");
+  };
+  
+  next.onclick = function() {
+    let activeSlide = arr.findIndex((item, idx) => item.className.includes('slider__item_active'));
+    arr[activeSlide].classList.toggle("slider__item_active");
+    activeSlide = (activeSlide == arr.length - 1) ? 0 : activeSlide + 1;;
+    arr[activeSlide].classList.toggle("slider__item_active");
+  };
